38d832282976f74a9adfb87b75920e8f655e4a44
Raghavendra <raghavendrakumar217@gmail.com>
2021-09-16 13:48:10 +0530

    js

---
+++ script.js
@@ -0,0 +1,25 @@
+// console.log("Hello world from file")
+// alert(10+50)
+// var name = prompt("Enter your name")
+// console.log(name)
+// document.write("This is another method...Dont Use")
+
+var a = 10
+var b = 20
+
+var str = "Hello world jvsuvidnbhj"
+
+var r
+
+const PI = 3.14
+// var st1 = " Hello "
+// var st2 = "Dhruv"
+a = 50
+b = 100
+// const x = 30
+// console.log(st1+st2+(a+b))
+
+str = 100000
+PI = 3.13
+r = 200
+console.log(PI)
