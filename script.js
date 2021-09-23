5dcebcf082a7198190cbbe06563268d18ba19dd7
Raghavendra <raghavendrakumar217@gmail.com>
2021-09-23 09:38:38 +0530

    new data

--- script.js
+++ script.js
@@ -46,26 +46,79 @@
 // }
 
 // // console.log(details.Name);
-// console.log(details.print());
-function sub()
-{
-   return 100-23
-}
-var a=sub()
+// // console.log(details.print());
+// function sub()
+// {
+//    return 100-23
+// }
+// var a=sub()
+// console.log(a);
+
+
+// function mul()
+// {
+//     console.log(2*a);
+// }
+// mul(a)
+// var per={
+//     name:"rahul",
+//     age:29,
+//     number:987655
+// }
+// console.log(per["age"]);
+// var a=10
+// var b=1222
+// // console.log(`${a} this is a nuber less then 11 ${b} this number is divided by 2`);
+
+
+// var str = "aman,raj,rahul,ajay"
+// var arr = str.split(",")
+// // console.log(arr);
+// var a="hello"
+// var b="world"
+// console.log(`${a} very bad ${b}`);
+// let a=1999.0000
+// b=parseFloat(a)
+// // console.log(b);
+
+// const arr = ['Ram',23,true,[1,2,3],{name:'amit',age:34,marks:[34,54,78,98,23]}]
+// const arr1 = [45,23,99,102,2,76,34]
+// console.log(arr[4].marks[2]);
+
+// arr[1] = 99
+// arr = [1,false,"Name"]
+// arr.push("New item")
+// delete arr[1]
+// const newArr = arr.concat(arr1)
+// const newArr = arr + arr1
+// console.log(arr1.sort(
+//     (a,b) => {return b-a}
+//     )); 
+
+// var newArr = arr1.map((item) => {
+//     return item*100;
+// })
+
+// console.log(newArr);
+
+// var ar=[123,345,"rahul",{name:"ghajni",agr:46,ph:900990990},(a,b)=>{
+//     return a+b
+//     // console.log("I am in function");
+// } ]
+// ar.push("Hello world")
+// ar.length
+// console.log(ar);
+// console.log(ar[4](1111,1111));
+var arra=["hello","world","i Am" ,"here","but","not","there"]
+// arra.splice(6,10,"hey","hii")
+// console.log(arra);
+// function add(a,b)
+// {
+//     return a+b;
+// }
+// console.log(add(10000000000000,99));
+var a=arra.sort()
 console.log(a);
+arra.reverse();
+console.log(arra);
 
-
-function mul()
-{
-    console.log(2*a);
-}
-mul(a)
-var per={
-    name:"rahul",
-    age:29,
-    number:987655
-}
-console.log(per["age"]);
-var a=10
-var b=1222
-console.log(`${a} this is a nuber less then 11 ${b} this number is divided by 2`);
